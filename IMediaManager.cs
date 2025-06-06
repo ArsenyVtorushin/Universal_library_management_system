f323798e483ea3e61fc0c13e0c1f49338b42704d
Arseny Vtorushi <vtorushinarseny@yandex.ru>
2025-06-06 22:31:12 +0500

    2. Интерфейсы

---
+++ IMediaManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal_library_management_system
+{
+    public interface IMediaManager<T>
+    {
+        void Add(T item);
+        bool Remove(string title);
+        T FindByTitle(string title);
+        IEnumerable<T> FilterByYear(int year);
+        IEnumerable<T> GetAllAvailable();
+    }
+
+}
