24c57b8851bad7c5c2c1df4ae939984798a855bd
Arseny Vtorushi <vtorushinarseny@yandex.ru>
2025-06-07 12:35:06 +0500

    3, 4. Класс Library

---
+++ Library.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal_library_management_system
+{
+    public class Library<T> : IMediaManager<T> where T : Media
+    {
+        private List<T> _media;
+        private Dictionary<string, T> _dictionary;
+
+        public Library(List<T> media)
+        {
+            _media = media;
+            _dictionary = new Dictionary<string, T>();
+
+            for (int i = 0; i < _media.Count; i++)
+                _dictionary.Add(_media[i].Title, _media[i]);
+        }
+
+
+        public void Show()
+        {
+            foreach (var item in _media)
+                Console.WriteLine($"Название: {item.Title}\nАвтор: {item.Author}\n" +
+                    $"Год публикации: {item.YearPublished}\nДоступность: {item.IsAvailable}\n");
+        }
+
+
+        public T this[int index]
+        {
+            get { return _media[index]; }
+            set { _media[index] = value; }
+        }
+
+
+        public void Add(T item)
+        {
+            _media.Add(item);
+        }
+
+
+        public IEnumerable<T> FilterByYear(int year)
+        {
+            return _media.Where(e => e.YearPublished == year);
+        }
+
+
+        public T FindByTitle(string title)
+        {
+            return _dictionary[title];
+        }
+
+
+        public IEnumerable<T> GetAllAvailable()
+        {
+            return _media.Where(e => e.IsAvailable);
+        }
+
+
+        public bool Remove(string title)
+        {
+            if (_dictionary.ContainsKey(title))
+            {
+                _media.Remove(_dictionary[title]);
+                _dictionary.Remove(title);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Book> books = new List<Book>()
+            {
+                new Book {Title = "Мастер и Маргарита", Author = "Михаил Булгаков",
+                    YearPublished = 1967, IsAvailable = true, PagesCount = 480, Genre = "Роман"},
+
+                new Book {Title = "Война и мир", Author = "Лев Толстой",
+                    YearPublished = 1869, IsAvailable = false, PagesCount = 1225, Genre = "Эпопея"},
+
+                new Book {Title = "Преступление и наказание", Author = "Фёдор Достоевский",
+                    YearPublished = 1866, IsAvailable = true, PagesCount = 544, Genre = "Психологический роман"},
+
+                new Book {Title = "Идиот", Author = "Фёдор Достоевский",
+                    YearPublished = 1869, IsAvailable = true, PagesCount = 608, Genre = "Социальная драма"},
+
+                new Book {Title = "Анна Каренина", Author = "Лев Толстой",
+                    YearPublished = 1878, IsAvailable = true, PagesCount = 864, Genre = "Роман"},
+
+                new Book {Title = "Евгений Онегин", Author = "Александр Пушкин",
+                    YearPublished = 1831, IsAvailable = true, PagesCount = 256, Genre = "Поэма"},
+
+                new Book {Title = "Мёртвые души", Author = "Николай Гоголь",
+                    YearPublished = 1842, IsAvailable = false, PagesCount = 384, Genre = "Поэма в прозе"},
+
+                new Book {Title = "Отцы и дети", Author = "Иван Тургенев",
+                    YearPublished = 1862, IsAvailable = true, PagesCount = 352, Genre = "Роман"},
+
+                new Book {Title = "Герой нашего времени", Author = "Михаил Лермонтов",
+                    YearPublished = 1840, IsAvailable = true, PagesCount = 288, Genre = "Роман"},
+
+                new Book {Title = "Обломов", Author = "Иван Гончаров",
+                    YearPublished = 1859, IsAvailable = false, PagesCount = 576, Genre = "Роман"},
+
+                new Book {Title = "Двенадцать стульев", Author = "Илья Ильф, Евгений Петров",
+                    YearPublished = 1928, IsAvailable = true, PagesCount = 352, Genre = "Комедия"}
+            };
+
+            var library = new Library<Book>(books);
+            library.Show();
+            
+
+            library.Add(new Book
+            {
+                Title = "451 градус по Фаренгейту",
+                Author = "Рэй Брэдбери",
+                YearPublished = 1953,
+                IsAvailable = true,
+                PagesCount = 240,
+                Genre = "Научная фантастика"
+            });
+
+            foreach (var item in library.FilterByYear(1953))
+                Console.WriteLine(item.Title);
+
+            library.Remove("451 градус по Фаренгейту");
+
+
+
+
+
         }
     }
 }
